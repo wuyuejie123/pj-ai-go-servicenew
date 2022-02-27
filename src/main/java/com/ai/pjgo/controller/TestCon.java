e766ea528adae21b6c052aa9782fa28a106d69bc
one_one_root <180054421@qq.com>
2022-02-27 19:56:42 +0800

    配置跨域 新建简单测试类

--- src/main/java/com/ai/pjgo/controller/TestCon.java
+++ src/main/java/com/ai/pjgo/controller/TestCon.java
@@ -4,21 +4,47 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author hexinxiang
  * <br/>Date: 2022/2/26
  * <br/>Time: 17:30
  */
 
-
+@ResponseBody
+@RequestMapping("/heihe")
 @Controller
 public class TestCon {
 
 
-    @ResponseBody
+
     @RequestMapping("/test")
     public String test1(){
 
         return "你牛鼻！";
     }
+
+
+    @RequestMapping("/login")
+    public String test2(String username, String password, HttpServletResponse rsp){
+
+
+        String mess="no";
+        String token="0";
+        if (username.equals(password)){
+            mess="yes";
+            token="123";
+        }else {
+            mess="no";
+            token="000";
+        }
+
+        rsp.setHeader("token",token);
+
+        return mess;
+    }
+
+
+
 }
